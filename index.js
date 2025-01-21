b349bfb3e70f1fa9b2e1d8b4c1aacf8851b4bebd
POONGUZHALI SECE <poonguzhali799@gmail.com>
2025-01-21 12:34:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function viewadd() {
+    const url = "https://poojana-cse.github.io/Advertisement-Project-MERN/";
+    window.open(url, "_blank");
+}
+function viewbox() {
+    const url = "https://poojana-cse.github.io/Magic-Box-MERN/";
+    window.open(url, "_blank");
+}
+function viewemojii() {
+    const url = "https://poojana-cse.github.io/Emoji-Project-MERN/";
+    window.open(url, "_blank");
+}
+function viewapi() {
+    const url = "https://poojana-cse.github.io/API-Project-MERN/";
+    window.open(url, "_blank");
+}
